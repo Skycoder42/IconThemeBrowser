4d196b9c1b3522510011174be60cd754c9dfeeba
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-01-11 19:19:59 +0100

    added copying

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,13 +6,22 @@
 #include <dialogmaster.h>
 #include <QDirIterator>
 #include <QProgressDialog>
+#include <QClipboard>
+#include <QMimeData>
 
 MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
-	ui(new Ui::MainWindow)
+	ui(new Ui::MainWindow),
+	countLabel(new QLabel(this))
 {
 	ui->setupUi(this);
 	ui->iconTreeView->sortByColumn(0, Qt::AscendingOrder);
+	ui->iconTreeView->addActions({
+									 ui->action_Copy_Icon_Name,
+									 ui->actionCopy_Name,
+									 ui->actionCopy_Icon
+								 });
+	statusBar()->addWidget(countLabel);
 
 	loadThemeNames();
 	on_currentThemeComboBox_activated(QIcon::themeName());
@@ -41,7 +50,7 @@ void MainWindow::on_currentThemeComboBox_activated(const QString &text)
 		addIcon(icon);
 	scanIcons(themes.value(text));
 
-	ui->statusBar->showMessage(tr("Total Icons: %1").arg(ui->iconTreeView->topLevelItemCount()));
+	updateCount();
 }
 
 void MainWindow::on_iconSizeSpinBox_valueChanged(int iconSize)
@@ -57,6 +66,58 @@ void MainWindow::on_filterLineEdit_textChanged(const QString &text)
 		item->setHidden(!matchItems.contains(item));
 	}
 }
+
+void MainWindow::on_searchIconsButton_clicked()
+{
+	auto max = themes.size();
+	auto values = themes.values();
+	auto progress = DialogMaster::createProgress(this, tr("Scanning all themes for icon names"), max);
+	progress->show();
+
+	for(auto i = 0; i < max; i++) {
+		progress->setValue(i);
+		QApplication::processEvents();
+		scanIcons(values[i]);
+	}
+	progress->setValue(max);
+
+	progress->deleteLater();
+	updateCount();
+}
+
+void MainWindow::on_iconTreeView_itemActivated(QTreeWidgetItem *item)
+{
+	doCopy(item);
+	statusBar()->showMessage(tr("Copied name and icon of %1!").arg(item->text(0)), 5000);
+}
+
+void MainWindow::on_action_Copy_Icon_Name_triggered()
+{
+	auto item = ui->iconTreeView->currentItem();
+	if(item) {
+		doCopy(item);
+		statusBar()->showMessage(tr("Copied name and icon of %1!").arg(item->text(0)), 5000);
+	}
+}
+
+void MainWindow::on_actionCopy_Name_triggered()
+{
+	auto item = ui->iconTreeView->currentItem();
+	if(item) {
+		doCopy(item, true, false);
+		statusBar()->showMessage(tr("Copied name of %1!").arg(item->text(0)), 5000);
+	}
+}
+
+void MainWindow::on_actionCopy_Icon_triggered()
+{
+	auto item = ui->iconTreeView->currentItem();
+	if(item) {
+		doCopy(item, false, true);
+		statusBar()->showMessage(tr("Copied icon of %1!").arg(item->text(0)), 5000);
+	}
+}
+
 
 void MainWindow::loadThemeNames()
 {
@@ -102,20 +163,20 @@ void MainWindow::scanIcons(const QString &basePath)
 	}
 }
 
-void MainWindow::on_searchIconsButton_clicked()
+void MainWindow::doCopy(QTreeWidgetItem *item, bool name, bool icon)
 {
-	auto max = themes.size();
-	auto values = themes.values();
-	auto progress = DialogMaster::createProgress(this, tr("Scanning all themes for icon names"), max);
-	progress->show();
-
-	for(auto i = 0; i < max; i++) {
-		progress->setValue(i);
-		QApplication::processEvents();
-		scanIcons(values[i]);
+	auto clip = QApplication::clipboard();
+	auto mime = new QMimeData();
+	if(name)
+		mime->setText(item->text(0));
+	if(icon) {
+		auto size = ui->iconSizeSpinBox->value();
+		mime->setImageData(item->icon(0).pixmap({size, size}).toImage());
 	}
-	progress->setValue(max);
+	clip->setMimeData(mime);
+}
 
-	progress->deleteLater();
-	ui->statusBar->showMessage(tr("Total Icons: %1").arg(ui->iconTreeView->topLevelItemCount()));
+void MainWindow::updateCount()
+{
+	countLabel->setText(tr("Total Icons: %1").arg(ui->iconTreeView->topLevelItemCount()));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,9 +1,11 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QLabel>
 #include <QMainWindow>
 #include <QMap>
 #include <QSet>
+#include <QTreeWidget>
 
 namespace Ui {
 class MainWindow;
@@ -22,17 +24,23 @@ private slots:
 	void on_currentThemeComboBox_activated(const QString &text);
 	void on_iconSizeSpinBox_valueChanged(int iconSize);
 	void on_filterLineEdit_textChanged(const QString &text);
-
 	void on_searchIconsButton_clicked();
+	void on_iconTreeView_itemActivated(QTreeWidgetItem *item);
+	void on_action_Copy_Icon_Name_triggered();
+	void on_actionCopy_Name_triggered();
+	void on_actionCopy_Icon_triggered();
 
 private:
 	Ui::MainWindow *ui;
+	QLabel *countLabel;
 	QMap<QString, QString> themes;
 	QSet<QString> iconNames;
 
 	void loadThemeNames();
 	bool addIcon(const QString &name);
 	void scanIcons(const QString &basePath);
+	void doCopy(QTreeWidgetItem *item, bool name = true, bool icon = true);
+	void updateCount();
 };
 
 #endif // MAINWINDOW_H
